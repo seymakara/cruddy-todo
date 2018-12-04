739e3f7411a1e0d5f0a51e9cfb1ee8c096bced74
Francis Distor <francis.distor@gmail.com>
2018-12-03 16:06:48 -0800

    Passing getNextUniqueId tests.

--- datastore/counter.js
+++ datastore/counter.js
@@ -38,9 +38,12 @@ const writeCounter = (count, callback) => {
 
 // Public API - Fix this function //////////////////////////////////////////////
 
-exports.getNextUniqueId = () => {
-  counter = counter + 1;
-  return zeroPaddedNumber(counter);
+exports.getNextUniqueId = (callback) => {
+  readCounter((err, data) => {
+    writeCounter(data + 1, (err, Data)=> {
+      callback(err, Data);
+    });
+  });
 };
 
 
